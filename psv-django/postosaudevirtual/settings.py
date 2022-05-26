b2911102c7502915ab92630dc97865446b115e4f
Robson <rma@a.palmares.ifpe.edu.br>
2022-05-26 01:29:56 -0300

    remove .env e edita settings.py

--- psv-django/postosaudevirtual/settings.py
+++ psv-django/postosaudevirtual/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-nq=x^&ewr_ok^m!gd0^y$2gfj@$+vhq^2#_*tf8g)xa#=&yrjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
