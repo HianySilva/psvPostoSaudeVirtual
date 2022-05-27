3885b7cfa1fac4a06ffb868ceae40a1e7b6d1d23
Robson <rma@a.palmares.ifpe.edu.br>
2022-05-27 17:44:29 -0300

    edita views.py de medico

--- medico/views.py
+++ medico/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Medico
 
-# Create your views here.
+class ListaMedicoView(ListView):
+    model = Medico
+    queryset = Medico.objects.all().order_by('nome_completo')
+
