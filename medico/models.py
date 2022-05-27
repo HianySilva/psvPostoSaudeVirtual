3f2d4ebeaa996a336c3cad55d43b383c6d1fe58c
Hiany <hgfs@discente.ifpe.edu.br>
2022-05-27 17:37:18 -0300

    edita models.py de medico

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+class Medico (models.Model):
+    nome_completo = models.CharField(max_length=256)
+    crm = models.CharField(max_length=256)
+    especializacao = models.CharField(max_length=256)
+    turno = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    senha = models.CharField(max_length=256)
