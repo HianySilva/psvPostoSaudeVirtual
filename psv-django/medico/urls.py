96035337ed039d978e5c69cf836e8eec90e10a07
Robson <rma@a.palmares.ifpe.edu.br>
2022-05-27 19:09:14 -0300

    edita urls.py

--- psv-django/medico/urls.py
+++ psv-django/medico/urls.py
@@ -1,7 +1,8 @@
 import imp
 from django.urls import path
-from .views import ListaMedicoView
+from .views import ListaMedicoView, MedicoCreateView
 
 urlpatterns = [
-    path('', ListaMedicoView.as_view(), name='medico.index')
+    path('', ListaMedicoView.as_view(), name='medico.index'),
+    path('novo/', MedicoCreateView.as_view(), name='medico.novo'),
 ]
--- psv-django/medico/views.py
+++ psv-django/medico/views.py
@@ -1,8 +1,14 @@
+import imp
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Medico
+from .forms import MedicoForm
 
 class ListaMedicoView(ListView):
     model = Medico
     queryset = Medico.objects.all().order_by('nome_completo')
 
+class MedicoCreateView(CreateView):
+    model = Medico
+    form_class = MedicoForm
+    success_url = '/medicos/'
