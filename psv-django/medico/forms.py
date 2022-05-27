02e0c572940137325c915724eacdf56c8994e43a
Robson <rma@a.palmares.ifpe.edu.br>
2022-05-27 19:08:34 -0300

    cria forms.py

---
+++ psv-django/medico/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import fields, models
+from .models import Medico
+
+class MedicoForm(forms.ModelForm):
+    class Meta:
+        model = Medico
+        fields = ('nome_completo','crm','especializacao','turno','email','senha')
