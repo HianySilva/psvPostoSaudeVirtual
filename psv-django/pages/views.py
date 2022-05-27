fe960c28c172b0ad6104aef569a6a3e1cbe2fae8
Priscila <pca@discente.ifpe.edu.br>
2022-05-27 17:07:29 -0300

    edita views.py

--- psv-django/pages/views.py
+++ psv-django/pages/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import Medico
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    class ListaMedicoView(ListView):
+        model = Medico
+        queryset = Medico.objects.all().order_by('nome_completo')
+
