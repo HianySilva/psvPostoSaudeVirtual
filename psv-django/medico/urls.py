9ecb620bdef4359a31b3a6aacaed993bbe86ce1f
Robson <rma@a.palmares.ifpe.edu.br>
2022-05-28 01:25:25 -0300

    back-end da tela de cadastro de médicos #7

--- psv-django/medico/urls.py
+++ psv-django/medico/urls.py
@@ -1,8 +1,10 @@
 import imp
 from django.urls import path
-from .views import ListaMedicoView, MedicoCreateView
+from .views import ListaMedicoView, MedicoCreateView, MedicoUpdateView, MedicoDeleteView
 
 urlpatterns = [
     path('', ListaMedicoView.as_view(), name='medico.index'),
     path('novo/', MedicoCreateView.as_view(), name='medico.novo'),
+    path('editar/<int:pk>', MedicoUpdateView.as_view(), name='medico.editar'),
+    path('remover/<int:pk>', MedicoDeleteView.as_view(), name='medico.remover'),
 ]
