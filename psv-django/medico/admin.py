72bf38e8070a5a98a5d81ad3621dacc59d787eff
Robson <rma@a.palmares.ifpe.edu.br>
2022-06-01 23:00:47 -0300

    edita admin.py

--- psv-django/medico/admin.py
+++ psv-django/medico/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Medico
 
 # Register your models here.
+
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'crm',
+        'especializacao',
+        'turno',
+        'email',
+        'senha'
+    ]
+
+    search_fields = [
+        'nome_completo'
+    ]
+
+
+admin.site.register(Medico, MedicoAdmin)
