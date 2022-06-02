f9a1603c233287da084f0d5cb961dd5292e86409
Robson <rma@a.palmares.ifpe.edu.br>
2022-06-01 22:51:43 -0300

    edita urls.py

--- psv-django/pages/urls.py
+++ psv-django/pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('funcionalidades/', views.FuncionalidadesView.as_view(), name='funcionalidades'),
-    path('cadastrar_agendar/', views.Consultar_agendarView.as_view(), name='cadastrar_agendar'),
+    path('funcionalidades/', login_required(views.FuncionalidadesView.as_view()), name='funcionalidades'),
+    path('cadastrar_agendar/', login_required(views.Consultar_agendarView.as_view()), name='cadastrar_agendar'),
 ]
