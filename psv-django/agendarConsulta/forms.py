837d5209230f0a15543df583b0d730c6b56e1f5b
Robson <rma@a.palmares.ifpe.edu.br>
2022-06-15 00:40:14 -0300

    Reseta o banco de dados

--- psv-django/agendarConsulta/forms.py
+++ psv-django/agendarConsulta/forms.py
@@ -10,4 +10,4 @@ class PessoaForm(forms.ModelForm):
     )
     class Meta:
         model = Pessoa
-        fields = ('numero_Do_Cartao_Do_SUS','cPF','data_De_Nascimento','telefone','rua', 'cEP', 'numero_Da_Residencia', 'bairro', 'complemento', 'referencia')
+        fields = ('numero_Do_Cartao_Do_SUS','cPF','data_De_Nascimento','endereco','numero_Da_Residencia', 'complemento', 'bairro', 'telefone', 'cEP', 'ponto_De_Referencia')
--- psv-django/agendarConsulta/models.py
+++ psv-django/agendarConsulta/models.py
@@ -4,13 +4,13 @@ class Pessoa(models.Model):
     numero_Do_Cartao_Do_SUS = models.CharField(max_length=256)
     cPF = models.CharField(max_length=256)
     data_De_Nascimento = models.DateField(null=True)
-    telefone = models.CharField(max_length=256)
-    rua = models.CharField(max_length=256)
-    cEP = models.CharField(max_length=256)
+    endereco = models.CharField(max_length=256)
     numero_Da_Residencia = models.CharField(max_length=256)
-    bairro = models.CharField(max_length=256)
     complemento = models.CharField(max_length=256)
-    referencia = models.TextField(max_length=256)
+    bairro = models.CharField(max_length=256)
+    telefone = models.CharField(max_length=256)
+    cEP = models.CharField(max_length=256)
+    ponto_De_Referencia = models.TextField(max_length=256)
 
     def __str__(self) -> str:
         return self.cPF
