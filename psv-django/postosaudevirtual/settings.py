43d3c82b66d90938aab74b1587ffd90d80a40196
Robson <rma@a.palmares.ifpe.edu.br>
2022-06-23 16:40:57 -0300

    Atualiza arquivo settings.py

--- psv-django/postosaudevirtual/settings.py
+++ psv-django/postosaudevirtual/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,4 +157,4 @@ ACCOUNT_UNIQUE_EMAIL = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
